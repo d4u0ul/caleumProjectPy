bd640bd02cf4225068fc9b6dc41e37a9902ff0ca
d4u0ul <ahb@ecomp.poli.br>
2022-10-22 18:02:41 -0300

    exceções

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # This is a sample Python script.
+from banco import ContaCorrente
+
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -9,8 +11,24 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def metodo1():
+    print('início	do	metodo1')
+    metodo2()
+    print('fim	do	metodo1')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def metodo2():
+    print('início	do	metodo2')
+    cc = ContaCorrente('José', '123')
+    for i in range(1, 15):
+        cc.deposita(i + 1000)
+        print(cc.saldo)
+        if (i == 5):
+            cc = None
+    print('fim	do	metodo2')
+
+
+if __name__ == '__main__':
+    print('início	do	main')
+    metodo1()
+    print('fim	do	main')
--- src/banco.py
+++ src/banco.py
@@ -1,6 +1,6 @@
 import abc  # import de  abstratos
 from abc import ABC
-
+from excecoes import SaldoInsuficienteError
 
 class Banco:
     pass
@@ -58,19 +58,6 @@ class Diretor(Funcionario):
     def __init__(self, nome, cpf, salario):
         super().__init__(nome, cpf, salario)
 
-
-class Conta(abc.ABC):
-    def __init__(self, numero, titular, saldo=0, limite=1000.0):
-        self._numero = numero
-        self._titular = titular
-        self._saldo = saldo
-        self._limite = limite
-
-    @abc.abstractmethod
-    def atualiza(self):
-        pass
-
-
 import datetime
 
 
@@ -86,6 +73,11 @@ class Historico:
             print("-", t)
 
 
+class Autenticavel:
+    def autentica(self, senha):
+        pass
+
+
 class Conta:
     def __init__(self, numero, titular, saldo=0, limite=1000.0):
         self._numero = numero
@@ -97,17 +89,27 @@ class Conta:
     @property
     def saldo(self):
         return self._saldo
+    @saldo.setter
+    def saldo(self,valor):
+        self._saldo = valor
 
     # código	omitido
     def deposita(self, valor):
-        self.saldo += valor
-        self.historico.transacoes.append("depósito	de	{}".format(valor))
+        if (self.saldo < valor):
+            raise ValueError("valor negativo")
+        else:
+            self.saldo +=valor
+            self.historico.transacoes.append("depósito	de	{}".format(valor))
 
     def saca(self, valor):
-        if (self.saldo < valor):
-            return False
+        print("sacando")
+
+        if (valor < 0):
+            raise ValueError('Você	tentou	sacar	um	valor	negativo.')
+        if (self._saldo < valor):
+            raise SaldoInsuficienteError('Saldo	insuficiente.')
         else:
-            self.saldo -= valor
+            self._saldo -= (valor + 0.10)
             self.historico.transacoes.append("saque	de	{}".format(valor))
 
     def extrato(self):
@@ -115,26 +117,27 @@ class Conta:
         self.historico.transacoes.ap
 
     @abc.abstractmethod
-    def atualiza(self):
+    def atualiza(self, valor):
         pass
 
 
-class ContaInvestimeto(Conta):
-    pass
+class ContaInvestimento(Conta):
+    def __init__(self, numero, titular, saldo=0, limite=1000.0):
+        super().__init__(numero, titular, saldo=0, limite=1000.0)
+        pass
+
+
+# verifica	se	a	senha	confere
 
 
 class ContaCorrente(Conta):
     def atualiza(self, valor):
         pass
-    pass
 
 
 class ContaPoupanca(Conta):
-    def atualiza(self, valor):
-        pass
-    pass
-
-class ContaInvestimento(Conta):
+    # def atualiza(self, valor):
+    #    pass
     pass
 
 
@@ -155,6 +158,12 @@ if __name__ == '__main__':
     cc = ContaCorrente('123-4', 'João', 1000.0)
     cp = ContaPoupanca('123-5', 'José', 1000.0)
     cc.atualiza(0.01)
+    try:
+        cc.saca(10000)
+    except ValueError:
+        print("valor err")
+    except SaldoInsuficienteError:
+        print ("Saldo insuficiente2")
     cp.atualiza(0.01)
     print(cc.saldo)
     print(cp.saldo)
